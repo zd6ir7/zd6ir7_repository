572afdbdc4bf99b1418f85595d5791ff3492ddd0
root <root@ip-172-31-9-118.ap-southeast-1.compute.internal>
2018-07-16 02:21:49 +0000

    add new file

--- javaapplication/MySQLDBAccess.java
+++ javaapplication/MySQLDBAccess.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication;
 
 import java.sql.Connection;
@@ -16,7 +11,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author haibou
+ * PreparedStatementクラスを使ってMySQLへアクセスするクラス
  */
 public class MySQLDBAccess {
     public static void main(String[] args) {
--- javaapplication/MySQLFalseDBAccess.java
+++ javaapplication/MySQLFalseDBAccess.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication;
 
 import java.sql.Connection;
@@ -17,7 +12,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author haibou
+ * Statementクラスを使ってMySQLへアクセスするクラス
  */
 public class MySQLFalseDBAccess {
     public static void main(String[] args) {
